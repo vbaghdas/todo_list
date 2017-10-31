8db593a13a033accbc7e728e80b964ea477e2063
vbaghdas <vbaghdas@gmail.com>
2017-10-30 19:07:00 -0700

    using BrowserRouter instead of HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import promise from './middleware/promise';
 import logger from './middleware/logger';
 import rootReducer from './reducers';
@@ -13,9 +13,9 @@ const createStoreWithMiddleWare = applyMiddleware(promise, logger)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleWare(rootReducer)}>
-        <HashRouter>
+        <Router>
             <App/>
-        </HashRouter>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
